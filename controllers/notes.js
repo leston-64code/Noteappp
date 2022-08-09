069034eafacceaec2fdb56b9b5ff10ad098fad0c
Leston <lestonair6574@gmail.com>
2022-08-10 00:17:42 +0530

    delete all notes done

--- controllers/notes.js
+++ controllers/notes.js
@@ -39,9 +39,46 @@ exports.getallnotes=async(req,res,next)=>{
        if(notes){
        return res.status(200).json({
             success:true,
+            count:notes.length,
+            notes,           
+        })
+       }
+      
+    } catch (error) {
+        next(error)
+    }
+    
+}
+exports.deleteallnotes=async(req,res,next)=>{
+    try {
+        const userID=req.header("userID")
+        if(userID==undefined){
+            return res.status(404).json({
+                success:false,
+                message:"Please login to get your notes"
+            })
+        }
+        let notes=await Notes.find({user:userID})
+        if(!notes){
+            return res.status(404).json({
+               success:false,
+               message:"No notes found"
+            })
+          }        
+
+       if(notes){
+        let count=notes.length
+        for (let k of notes){
+            notes=await Notes.findOneAndRemove(k._id)
+        }
+
+       return res.status(200).json({
+            success:true,
+            count,
             notes
         })
        }
+       
       
     } catch (error) {
         next(error)
--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,15 @@
 const express=require("express")
 const router=express.Router()
 
-const {addnote,getallnotes,updatenote,deletenote}=require("../controllers/notes")
+const {addnote,getallnotes,updatenote,deletenote,deleteallnotes}=require("../controllers/notes")
 
 router.route("/addnote").post(addnote)
 
 
 router.route("/getallnotes").post(getallnotes)
+
+
+router.route("/deleteallnotes").post(deleteallnotes)
 
 
 router.route("/updatenote").post(updatenote)
